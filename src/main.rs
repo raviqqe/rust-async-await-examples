e0f5bf7a85fafa782a7ee3689e8dfd72a34d312f
Yota Toyama <raviqqe@gmail.com>
2018-01-13 12:26:56 +0900

    Use cpu pool

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,36 @@
 #![feature(proc_macro, conservative_impl_trait, generators)]
 
 extern crate futures_await as futures;
+extern crate futures_cpupool;
+#[macro_use]
+extern crate lazy_static;
 
 use futures::prelude::*;
+use futures_cpupool::*;
+
+lazy_static! {
+    static ref POOL: CpuPool = CpuPool::new_num_cpus();
+}
 
 #[async]
 fn foo() -> Result<i32, i32> {
-    Ok(1 + await!(bar())?)
+    loop {}
 }
 
 #[async]
 fn bar() -> Result<i32, i32> {
-    Ok(2)
+    let f1 = foo();
+    let f2 = foo();
+
+    let c1 = POOL.spawn(f1);
+    let c2 = POOL.spawn(f2);
+
+    c1.wait()?;
+    c2.wait()?;
+
+    return Ok(1);
 }
 
 fn main() {
-    println!("{:#?}", foo().wait());
+    POOL.spawn(bar()).wait();
 }
